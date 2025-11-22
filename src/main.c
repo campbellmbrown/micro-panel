a6df07212261adeae2343a326016b44101496023
Campbell Brown <campbellbrown093@gmail.com>
2025-11-23 09:08:52 +1300

    Makefile to cmake

--- src/main.c
+++ src/main.c
@@ -7,10 +7,16 @@
 int main(void)
 {
     nrf_gpio_cfg_output(LED_1);
+    nrf_gpio_cfg_output(LED_2);
+    nrf_gpio_cfg_output(LED_3);
 
     while (true)
     {
         nrf_gpio_pin_toggle(LED_1);
+        nrf_delay_ms(500);
+        nrf_gpio_pin_toggle(LED_2);
+        nrf_delay_ms(500);
+        nrf_gpio_pin_toggle(LED_3);
         nrf_delay_ms(500);
     }
 }
